e53bf6bdb331c48a9dca780b5a750aacda3b968c
Gabriel Soares <gabriel-soares@uergs.edu.br>
2023-07-21 23:13:08 -0300

    v0.1.10

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -3,6 +3,6 @@ import { AuthModule } from './auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(5001, '0.0.0.0');
+  await app.listen(5001);
 }
 bootstrap();
