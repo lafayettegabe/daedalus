0f4fffe20786b53698ae017096a418951c53ab46
Gabriel Soares <gabriel-soares@uergs.edu.br>
2023-07-21 22:52:51 -0300

    v0.1.8

--- apps/template/src/main.ts
+++ apps/template/src/main.ts
@@ -3,6 +3,6 @@ import { TemplateModule } from './template.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(TemplateModule);
-  await app.listen(5002);
+  await app.listen(5002, '0.0.0.0');
 }
 bootstrap();
